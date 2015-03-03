30aa7203ebd08cdc4bc506a862f4d0812e4476f3
Antonio Gutierrez <chibby0ne@gmail.com>
2015-03-03 12:18:45 +0100

    Add install script and update .tmux.conf
    Install script includes steps for installing vim and tmux in case it is
    not installed, as well as creating folders and symbolic links for
    configuration files. Additionally, installs Vundle plugin for vim, but
    let's the patch application for the user. Tmux has a new binding and
     new settings for status bar.

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# installing vim and tmux
+echo "================="
+echo "Installing vim and tmux"
+echo "================="
+sudo apt-get update && sudo apt-get install vim tmux
+echo -e "Vim and tmux installed!\n"
+echo
+
+echo "================="
+echo "Setting symlinks"
+echo "================="
+echo "Setting bashrc"
+ln -s bashrc ~/.bashrc
+echo "Setting vimrc"
+ln -s vimrc ~/.vimrc
+echo -e "Setting tmux.conf\n"
+ln -s tmux.conf ~/.tmux.conf
+echo
+
+echo "================="
+echo "Setting up .tmux.conf.dev" 
+echo "================="
+echo "Creating ~/.tmux"
+mkdir ~/.tmux
+echo -e "Setting symlink\n"
+ln -s tmux/tmux.conf.dev ~/.tmux/.tmux.conf.dev 
+
+echo "================="
+echo "Installing Vundle" 
+echo "================="
+echo "Creating ~/.vim/bundle"
+mkdir ~/.vim/bundle
+echo "Cloning Vundle"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo -e "Vundle installed!\n"
+
+echo "NOTE: Remember to open vim and run :PluginInstall. in order to \ 
+install the rest of the plugins"
+
+
+
