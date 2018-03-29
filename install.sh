c156b614eaaee99442c0eadfbe9fbc66f47b0c5b
Antonio Gutierrez <chibby0ne@gmail.com>
2018-03-29 12:59:41 +0200

    install.sh: refactor and fix raspbian/debian issues
    - The previous distro string used to contain "GNU/Linux" for
    Debian/Raspbian, and this was more than needed for the comparison and
    for the docker download link.
    
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- install.sh
+++ install.sh
@@ -1,104 +1,131 @@
 #!/bin/bash
 
+# $1: status of last command
+# $2: succesful message
+# $3: failure message
+# $4: whether to exit script or not. default don't exit
+function printMessage() {
+    if [[ $1 -eq 0 ]]; then
+        echo -e $2
+    else
+        echo -e $3
+        if [[ -n "$4" && "$4" == "EXIT" ]]; then
+            exit 1
+        fi
+    fi
+}
+
 # Get script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "================="
 echo "Installing several very useful packages (needs sudo)"
 echo "================="
-distro=$(cat /etc/*-release | grep ^NAME= | sed 's/NAME="\(.*\)"/\1/g')
-if [[ "$distro" == "Arch Linux" ]]; then
+distro=$(cat /etc/*-release | grep ^NAME= | sed -E 's/NAME="(\w+)\s+\w+"/\1/g' | tr "[:upper:]" "[:lower:]")
+if [[ "$distro" == "arch" ]]; then
     sudo pacman -Syyu --noconfirm && sudo pacman -S vim tmux dmidecode ruby rubygems rust go python docker curl powerline-fonts ttf-dejavu noto-fonts --noconfirm
-elif [[ "$distro" == "Ubuntu" ]]; then
-    sudo apt-get update && sudo apt-get install vim tmux dmidecode ruby-dev rubygems rust python -y
+elif [[ "$distro" == "ubuntu" || "$distro" == "debian" || "$distro" == "raspbian" ]]; then
+    sudo apt update && sudo apt install vim tmux dmidecode ruby-dev libgemplugin-ruby rustc python -Vy
     # Taken from dockers docs: https://docs.docker.com/install/linux/docker-ce/ubuntu/#set-up-the-repository
-    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install docker-ce -y
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -Vy
+    if [[ "$distro" == "raspbian" ]]; then
+        distro="debian"
+    fi
+    curl -fsSL https://download.docker.com/linux/$distro/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$distro  $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt install docker-ce -Vy
 fi
-echo -e "Vim and tmux installed!\n"
+printMessage $? "All programs installed\n" "Error installing all the programs\n" "EXIT"
 
 echo "================="
 echo "Setting symlinks"
 echo "================="
-echo "Setting bashrc"
-ln -sf ${SCRIPT_DIR}/bashrc ~/.bashrc
-echo "Setting zshrc"
-ln -sf ${SCRIPT_DIR}/zshrc ~/.zshrc
-echo "Setting Xdefaults"
-ln -sf ${SCRIPT_DIR}/Xdefaults ~/.Xdefaults
+for file in bashrc zshrc Xdefaults vimrc gitconfig tern-config profile; do
+    echo "Setting $file"
+    ln -sf ${SCRIPT_DIR}/$file ~/.$file
+    printMessage $? "$file symlink set" "Failed to set $file symlink"
+done
+
+# echo "Setting bashrc"
+# ln -sf ${SCRIPT_DIR}/bashrc ~/.bashrc
+# echo "Setting zshrc"
+# ln -sf ${SCRIPT_DIR}/zshrc ~/.zshrc
+# echo "Setting Xdefaults"
+# ln -sf ${SCRIPT_DIR}/Xdefaults ~/.Xdefaults
 echo "Setting .config/i3/config"
 mkdir -p ~/.config/i3 && ln -sf ${SCRIPT_DIR}/i3/config ~/.config/i3/config
-echo "i3 lockscreen picture"
+printMessage $? ".config/i3/config symlink set" "Failed to set .config/i3/config symlink"
+echo "Setting i3 lockscreen picture"
 mkdir -p ~/Pictures && ln -sf ${SCRIPT_DIR}/i3/marscolonization.png ~/Pictures/marscolonization.png
-echo "Setting vimrc"
-ln -sf ${SCRIPT_DIR}/vimrc ~/.vimrc
-echo -e "Setting tmux.conf"
-ln -sf ${SCRIPT_DIR}/tmux.conf ~/.tmux.conf
-echo -e "Setting gitconfig"
-ln -sf ${SCRIPT_DIR}/gitconfig ~/.gitconfig
-echo -e "Setting global tern-config (required for js completion in vim with YCM)"
-ln -sf ${SCRIPT_DIR}/tern-config ~/.tern-config
-echo -e "Setting profile\n"
-ln -sf ${SCRIPT_DIR}/profile ~/.profile
+printMessage $? "i3 lock screen picture symlink set" "Failed to set i3 lockscreen picture symlink"
+echo "Setting .tmux/tmux.conf.dev"
+mkdir ~/.tmux && ln -sf ${SCRIPT_DIR}/tmux/tmux.conf.dev ~/.tmux/.tmux.conf.dev
+printMessage $? "i3 lock screen picture symlink set\n" "Failed to set i3 lockscreen picture symlink\n"
+# echo "Setting vimrc"
+# ln -sf ${SCRIPT_DIR}/vimrc ~/.vimrc
+# echo -e "Setting tmux.conf"
+# ln -sf ${SCRIPT_DIR}/tmux.conf ~/.tmux.conf
+# echo -e "Setting gitconfig"
+# ln -sf ${SCRIPT_DIR}/gitconfig ~/.gitconfig
+# echo -e "Setting global tern-config (required for js completion in vim with YCM)"
+# ln -sf ${SCRIPT_DIR}/tern-config ~/.tern-config
+# echo -e "Setting profile\n"
+# ln -sf ${SCRIPT_DIR}/profile ~/.profile
 
 
 # Find computer model to see if the scripts for handling fn keys are required
 laptop_model=$(sudo dmidecode | grep 'Version: ' | head -n 1 | sed 's/Version: \(.*\)/\1/g' | sed 's/[[:blank:]]//g')
 
 # Copy the scripts for making Fn keys work in Arch Linux for Asus Zenbook UX330 Notebook
-if [[ "$distro" == "Arch Linux" && "$laptop_model" == "UX330UAK.301" ]]; then
+if [[ "$distro" == "arch" && "$laptop_model" == "UX330UAK.301" ]]; then
     echo "================="
     echo "Setting symlinks and binaries for handling function keys in Asus Zenbook Laptop (needs sudo)"
     echo "================="
     echo "Setting symlink kb-light.py (Keyboard Backlight control)"
     ln -sf ${SCRIPT_DIR}/i3/kb-light.py ~/.config/i3/kb-light.py
+    printMessage $? "kb-light.py symlink set" "Failed to set kb-light.py symlink"
     echo "Setting symlink toogletouchpad.sh (Toogle touchpad control)"
     ln -sf ${SCRIPT_DIR}/i3/toogletouchpad.sh ~/.config/i3/toogletouchpad.sh
+    printMessage $? "toogletouchpad.sh symlink set" "Failed to set toogletouchpad.sh symlink"
     echo "Setting adjust_brightness (Screen Brightness control)"
     sudo cp ${SCRIPT_DIR}/i3/adjust_brightness /usr/local/bin/
+    printMessage $? "adjust_brightness installed\n" "Failed to install adjust_brightness\n"
 fi
 
-echo "================="
-echo "Setting up .tmux.conf.dev"
-echo "================="
-echo "Creating ~/.tmux"
-mkdir ~/.tmux
-echo -e "Setting symlink\n"
-ln -sf ${SCRIPT_DIR}/tmux/tmux.conf.dev ~/.tmux/.tmux.conf.dev
+# echo "================="
+# echo "Setting up .tmux.conf.dev"
+# echo "================="
+# echo "Creating ~/.tmux"
+# mkdir ~/.tmux
+# echo -e "Setting symlink\n"
+# ln -sf ${SCRIPT_DIR}/tmux/tmux.conf.dev ~/.tmux/.tmux.conf.dev
 
 echo "================="
 echo "Configuring Vim"
 echo "================="
 echo "Creating ~/.vim/bundle"
 mkdir -p ~/.vim/bundle
-echo "Installing Vundle"
+printMessage $? "~/.vim/bundle created" "Failed to create ~/.vim/bundle"
+echo "Cloning Vundle"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+printMessage $? "Vundle cloned" "Failed to clone Vundle"
 echo "Installing all other vim plugins listed in .vimrc"
 vim -c ":PluginInstall" -c ":q"
+printMessage $? "vim plugins installed" "Failed to install vim plugins"
 echo "Installing YouCompleteMe"
 cd ~/.vim/bundle/YouCompleteMe/ && ./install.py --all
-echo -e "All Vim plugins installed!\n"
+printMessage $? "YouCompleteMe installed\n" "Failed to install YouCompleteMe\n"
 
 echo "================="
 echo "Configuring Ruby Gems"
 echo "================="
-echo "Installing bundler"
-gem install bundler
-echo "Installing jekyll"
-gem install jekyll
-echo -e "All ruby gems installed\n"
-
-
-# echo "================="
-# echo "Downloading fanstasque font"
-# echo "================="
-# echo "downloading font to ~/fonts"
-#wget http://openfontlibrary.org/assets/downloads/fantasque-sans-mono/db52617ba875d08cbd8e080ca3d9f756/fantasque-sans-mono.zip -P ~/fonts/
-# echo -e "font downloaded\n"
-
-# echo "NOTE: Remember to open vim and run :PluginInstall in order to \
-# install the rest of the plugins. Also don't forget to use the patches once \
-# the plugins are installed"
+for app in bundler jekyll; do
+    echo "Installing $app"
+    gem install $app
+    printMessage "$app installed" "Error installing $app"
+done
+# echo "Installing bundler"
+# gem install bundler
+# echo "Installing jekyll"
+# gem install jekyll
