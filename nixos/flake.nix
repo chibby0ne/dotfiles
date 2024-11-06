09b1c5692317e8260117ad857e7d36c299c14e82
Antonio Gutierrez <chibby0ne@gmail.com>
2024-11-06 23:02:26 +0100

    nixos: add flake.{nix,lock} to move it to a flake setup
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

---
+++ nixos/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "NixOS Configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in
+  {
+    nixosConfigurations = {
+      earth = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit system;
+        };
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+  };
+
+}
