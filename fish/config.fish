59973fbdf782f1372418bb3817a93686db17a025
Antonio Gutierrez <chibby0ne@gmail.com>
2025-10-21 21:46:10 +0200

    fish: set useful env vars for locale
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,12 @@ if status is-interactive
 
     set -x EDITOR nvim
 
-    # Temperature in Celsius
-    set -x LC_MEASUREMENT en_gb.UTF-8
-
     # LANG
-    set -x LANG en_us.UTF-8
+    set -x LC_ALL en_US.UTF-8
+    set -x LC_CTYPE en_US.UTF-8
+    set -x LANG en_US.UTF-8
+    # Temperature in Celsius
+    set -x LC_MEASUREMENT en_GB.UTF-8
 
     # LS_COLORS is needed to have tree output colorized
     eval (dircolors -c ~/.config/dircolors)
